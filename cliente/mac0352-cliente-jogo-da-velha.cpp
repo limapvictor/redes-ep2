44495c253f9cdef728baf16cedb31285904596c7
Victor Lima <lima.p.victor@outlook.com>
2021-06-18 00:15:17 -0300

    Adicione checagem do resultado do jogo

--- cliente/mac0352-cliente-jogo-da-velha.cpp
+++ cliente/mac0352-cliente-jogo-da-velha.cpp
@@ -163,8 +163,41 @@ string getPlayerInPosition(int line, int column) {
     return getSymbolFromInt(player);
 }
 
+int isSequenceComplete(int x, int y, int z) {
+    if (x == y && y == z) return x;
+    return 0;
+}
+
+int getGameCurrentResult() {
+    int posA, posB, posC, posD, posE, posF, posG, posH, posI;
+    int lines, columns, diagonal1, diagonal2;
+
+    posA = fmod(board / pow(3, 0), 3);
+    posB = fmod(board / pow(3, 1), 3);
+    posC = fmod(board / pow(3, 2), 3);
+    posD = fmod(board / pow(3, 3), 3);
+    posE = fmod(board / pow(3, 4), 3);
+    posF = fmod(board / pow(3, 5), 3);
+    posG = fmod(board / pow(3, 6), 3);
+    posH = fmod(board / pow(3, 7), 3);
+    posI = fmod(board / pow(3, 8), 3);
+
+    lines = isSequenceComplete(posA, posB, posC) + isSequenceComplete(posD, posE, posF) + isSequenceComplete(posG, posH, posI);
+    if (lines > 0) return lines;
+    columns = isSequenceComplete(posA, posD, posG) + isSequenceComplete(posB, posE, posH) + isSequenceComplete(posC, posF, posI);
+    if (columns > 0) return columns;
+    diagonal1 = isSequenceComplete(posA, posE, posI);
+    if (diagonal1 > 0) return diagonal1;
+    diagonal2 = isSequenceComplete(posC, posE, posG);
+    if (diagonal2 > 0) return diagonal2;
+
+    if (posA > 0 && posB > 0 && posC > 0 && posD > 0 && posE > 0 && posF > 0 && posG > 0 && posH > 0 && posI > 0) return 0;
+    return -1;
+}
+
 void updateBoard(string player, int line, int column) {
     board += pow(3, (3 * (line - 1) + (column - 1))) * (SYMBOL_TO_INT[player]);
+    std::cout << "Tabuleiro atualizado: " << std::endl;
 }
 
 void printBoard() {
@@ -277,16 +310,19 @@ void waitForOpponentPlay() {
 
     std::cout << "Esperando jogada do adversário..." << std::endl;
     if ( (n = read(p2pFD, buffer, MAXLINE)) > 0) {
+        std::cout << "Jogada recebida!" << std::endl;
         buffer[n] = '\0';
         response = convertAndSplit(buffer);
         updateBoard(currentOpponentChar, stoi(response[1]), stoi(response[2]));
         printBoard();
+        std::cout << "Resultado do jogo: " << getGameCurrentResult() << std::endl;
     }
 }
 
 void handleSendCommand(vector<string> command, string fullCommand) {
     updateBoard(currentGameChar, stoi(command[1]), stoi(command[2]));
     printBoard();
+    std::cout << "Resultado do jogo: " << getGameCurrentResult() << std::endl;
     write(p2pFD, fullCommand.c_str(), fullCommand.length());
     waitForOpponentPlay();
 }
@@ -303,6 +339,7 @@ void handleGame(bool firstToPlay) {
         vector<string> command;
         string fullCommand;
         
+        std::cout << PROMPT;
         getline(std::cin, fullCommand);
         command = convertAndSplit(fullCommand.data());
 
