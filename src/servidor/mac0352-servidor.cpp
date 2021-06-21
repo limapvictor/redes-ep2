48ce54fb8d1d473b26a07be16adcfd897418a3e2
João Gabriel Lembo <joaogabriellembo@usp.br>
2021-06-21 05:37:13 -0300

    Remove comentarios servidor

--- src/servidor/mac0352-servidor.cpp
+++ src/servidor/mac0352-servidor.cpp
@@ -173,19 +173,17 @@ vector<string> get_SSL_read(SSL_CTX* ctx, int sockfd, char* recvline) {
 }
 
 int main (int argc, char **argv) {
-    /* Os sockets. Um que será o socket que vai escutar pelas conexões
-     * e o outro que vai ser o socket específico de cada conexão */
+    
     int listenfd, connfd;
-    /* Informações sobre o socket (endereço e porta) ficam nesta struct */
+    
     struct sockaddr_in servaddr;
     struct sockaddr_in clientaddr;
     socklen_t clientaddr_size = sizeof(struct sockaddr_in);
-    /* Retorno da função fork para saber quem é o processo filho e
-     * quem é o processo pai */
+    
     pid_t childpid;
-    /* Armazena linhas recebidas do cliente */
+    
     char recvline[MAXLINE + 1] = {};
-    /* Armazena o tamanho da string lida do cliente */
+    
     ssize_t n, hb_n;
 
     signal(SIGINT, server_stop);
@@ -204,27 +202,11 @@ int main (int argc, char **argv) {
         std::exit(1);
     }
 
-    /* Criação de um socket. É como se fosse um descritor de arquivo.
-     * É possível fazer operações como read, write e close. Neste caso o
-     * socket criado é um socket IPv4 (por causa do AF_INET), que vai
-     * usar TCP (por causa do SOCK_STREAM), já que o MQTT funciona sobre
-     * TCP, e será usado para uma aplicação convencional sobre a Internet
-     * (por causa do número 0) */
     if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("socket :(\n");
         std::exit(2);
     }
 
-    /* Agora é necessário informar os endereços associados a este
-     * socket. É necessário informar o endereço / interface e a porta,
-     * pois mais adiante o socket ficará esperando conexões nesta porta
-     * e neste(s) endereços. Para isso é necessário preencher a struct
-     * servaddr. É necessário colocar lá o tipo de socket (No nosso
-     * caso AF_INET porque é IPv4), em qual endereço / interface serão
-     * esperadas conexões (Neste caso em qualquer uma -- INADDR_ANY) e
-     * qual a porta. Neste caso será a porta que foi passada como
-     * argumento no shell (atoi(argv[1]))
-     */
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family      = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -234,10 +216,6 @@ int main (int argc, char **argv) {
         std::exit(3);
     }
 
-    /* Como este código é o código de um servidor, o socket será um
-     * socket passivo. Para isto é necessário chamar a função listen
-     * que define que este é um socket de servidor que ficará esperando
-     * por conexões nos endereços definidos na função bind. */
     if (listen(listenfd, LISTENQ) == -1) {
         perror("listen :(\n");
         std::exit(4);
@@ -246,35 +224,17 @@ int main (int argc, char **argv) {
     printf("[Servidor no ar. Aguardando conexões na porta %s]\n",argv[1]);
     printf("[Para finalizar, pressione CTRL+c ou rode um kill ou killall]\n");
    
-    /* O servidor no final das contas é um loop infinito de espera por
-     * conexões e processamento de cada uma individualmente */
 	for (;;) {
-        /* O socket inicial que foi criado é o socket que vai aguardar
-         * pela conexão na porta especificada. Mas pode ser que existam
-         * diversos clientes conectando no servidor. Por isso deve-se
-         * utilizar a função accept. Esta função vai retirar uma conexão
-         * da fila de conexões que foram aceitas no socket listenfd e
-         * vai criar um socket específico para esta conexão. O descritor
-         * deste novo socket é o retorno da função accept. */
+
         if ((connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) == -1 ) {
             perror("accept :(\n");
             std::exit(5);
         }
-        /* Agora o servidor precisa tratar este cliente de forma
-         * separada. Para isto é criado um processo filho usando a
-         * função fork. O processo vai ser uma cópia deste. Depois da
-         * função fork, os dois processos (pai e filho) estarão no mesmo
-         * ponto do código, mas cada um terá um PID diferente. Assim é
-         * possível diferenciar o que cada processo terá que fazer. O
-         * filho tem que processar a requisição do cliente. O pai tem
-         * que voltar no loop para continuar aceitando novas conexões.
-         * Se o retorno da função fork for zero, é porque está no
-         * processo filho. */
+        
         if ( (childpid = fork()) == 0) {
             /**** PROCESSO FILHO ****/
             printf("[Uma conexão aberta]\n");
-            /* Já que está no processo filho, não precisa mais do socket
-             * listenfd. Só o processo pai precisa deste socket. */
+           
             close(listenfd);
 
             signal(SIGINT, SIG_DFL);
@@ -313,23 +273,6 @@ int main (int argc, char **argv) {
             }
          
             write(connfd, "success", 7);
-            /* Agora pode ler do socket e escrever no socket. Isto tem
-             * que ser feito em sincronia com o cliente. Não faz sentido
-             * ler sem ter o que ler. Ou seja, neste caso está sendo
-             * considerado que o cliente vai enviar algo para o servidor.
-             * O servidor vai processar o que tiver sido enviado e vai
-             * enviar uma resposta para o cliente (Que precisará estar
-             * esperando por esta resposta) 
-             */
-
-            
-            /* ========================================================= */
-            /* ========================================================= */
-            /*                         EP1 INÍCIO                        */
-            /* ========================================================= */
-            /* ========================================================= */
-            /* TODO: É esta parte do código que terá que ser modificada
-             * para que este servidor consiga interpretar comandos MQTT  */
 
             fcntl(connfd, F_SETFL, O_NONBLOCK);
 
@@ -609,30 +552,15 @@ int main (int argc, char **argv) {
                     current_user->is_playing = true;
                 }
             }
-            /* ========================================================= */
-            /* ========================================================= */
-            /*                         EP1 FIM                           */
-            /* ========================================================= */
-            /* ========================================================= */
 
             log_client_disconnected(ip_addr);
 
-            // sd = SSL_get_fd(ssl);       /* get socket connection */
-            // SSL_free(ssl);         /* release SSL state */
-            // close(sd);
-
-            // SSL_CTX_free(ctx);
-            /* Após ter feito toda a troca de informação com o cliente,
-             * pode finalizar o processo filho */
             printf("[Uma conexão fechada]\n");
             close(connfd);
             std::exit(0);
         }
         else
             /**** PROCESSO PAI ****/
-            /* Se for o pai, a única coisa a ser feita é fechar o socket
-             * connfd (ele é o socket do cliente específico que será tratado
-             * pelo processo filho) */
             close(connfd);
     }
     std::exit(0);
